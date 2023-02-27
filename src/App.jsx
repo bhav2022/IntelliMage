7601680407786e28150ebbd62968a60fe0e86607
bhav2022 <118679553+bhav2022@users.noreply.github.com>
2023-02-27 23:44:42 +0530

    build 2

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,17 @@ const App = () => {
     <BrowserRouter>
       <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
         <Link to={"/"}>
-          <img src={logo} alt="Logo" className="object-contain w-36" />
+          <img src={logo} alt="Logo" className="object-contain w-44" />
         </Link>
-        <div>
+        <div className="flex flex-col gap-1">
           <Link
             to={"/create-post"}
-            className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md mr-3">
+            className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md mr-3 w-full">
             Create
           </Link>
           <Link
             to={"/"}
-            className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md sm:hidden">
+            className="font-inter font-medium bg-[#6469ff] text-white block px-4 py-2 rounded-md w-full">
             Community Showcase
           </Link>
         </div>
